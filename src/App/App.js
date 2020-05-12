c3584e2e0a9e74c3a85cb89c8265866ad75d6587
Joey Petrone <joey.petrone@gmail.com>
2020-05-11 20:35:37 -0500

    goats displaying

--- src/App/App.js
+++ src/App/App.js
@@ -2,21 +2,27 @@ import React from 'react';
 import './App.scss';
 
 import goatData from '../helpers/data/goatData';
+import GoatCoral from '../components/GoatCoral/GoatCoral';
 
 class App extends React.Component {
+  state = {
+    goats: [],
+  }
   // outside render - anything that modifies state
 
   componentDidMount() {
     const goats = goatData.getGoats();
-    console.error('goats', goats);
+    this.setState({ goats });
   }
 
   render() {
     // inside render - anything we need to do to modify the UI
+    const { goats } = this.state;
+
     return (
       <div className="App">
-        <h2 className="goat-title">Goats</h2>
-        <button className="btn btn-info">Goats Button</button>
+        <h3>GOAT YOGA LTD</h3>
+        <GoatCoral goats={goats}/>
       </div>
     );
   }
